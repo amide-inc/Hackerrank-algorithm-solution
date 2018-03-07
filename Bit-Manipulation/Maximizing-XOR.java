2dba6396ddf2e26244f06a5ade887c3fd4870411
Aamin <aminuddin01697@gmail.com>
2018-03-08 00:00:16 +0530

    Create Maximizing-XOR.java

---
+++ Bit-Manipulation/Maximizing-XOR.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int maximizingXor(int l, int r) {
+        int max = 0;
+        for(int i=l;i<=r;i++){
+            for(int j=i;j<=r;j++){
+              
+                if((i^j)>max){
+                    max = i^j;
+                }
+                }
+            }
+        
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        int r = in.nextInt();
+        int result = maximizingXor(l, r);
+        System.out.println(result);
+        in.close();
+    }
+}
